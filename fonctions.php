4b68c5b2e80fb7aa44ca15819ca031fe8d097b9e
levasseurl <levasseurlinfo7@gmail.com>
2021-01-06 22:09:20 +0100

    Ajout de la fonction de la route POST /candidature

--- fonctions.php
+++ fonctions.php
@@ -8,6 +8,7 @@ ACCUEIL
 
 */
 
+// Fonction de la route / 
 Flight::route('/', function(){
     // On vérifie si l'utilisateur est connecté,
     if (isset($_SESSION['utilisateur'])){
@@ -32,7 +33,7 @@ CONNEXION
 
 */
 
-
+// Fonction de la route /connexion avec la méthode GET
 Flight::route('GET /connexion', function(){
     // On affiche seulement le template de la page de connexion connexion.tpl
     Flight::render("connexion.tpl",NULL);
@@ -105,10 +106,12 @@ Flight::route("GET /inscription", function(){
 Flight::route("POST /inscription", function(){
     $messages_erreur = array();
 
+    // Vérification du champ nom
     if(empty($_POST['nom'])){
         $messages_erreur['nom'] = "Le nom d'utilisateur est obligatoire";
     }
 
+    // Vérification du champ email
     if(empty($_POST['email'])) {
         $messages_erreur['email'] = "L'adresse mail est obligatoire";
     }
@@ -123,6 +126,7 @@ Flight::route("POST /inscription", function(){
         }
     }
 
+    // Vérification du champ password
     if(empty($_POST['password'])) {
         $messages_erreur['password'] = "Le mot de passe est obligatoire";
     }
@@ -130,10 +134,12 @@ Flight::route("POST /inscription", function(){
         $messages_erreur['password'] = "Le mot de passe doit faire minimum 8 caracteres";
     }
 
+    // Envoi de messages d'erreurs si fautes de saisies
     if(!empty($messages_erreur)) {
         Flight::view()->assign("messages", $messages_erreur);
         Flight::view()->assign($_POST);
         Flight::render("inscription.tpl", NULL);
+    // Sinon envoi des informations saisies dans le formulaire vers la table utilisateur de la base de données
     } else {
         $get_db = Flight::get('db');
         $st = $get_db->prepare('INSERT INTO utilisateur VALUES(?,?,?,0)');
@@ -144,7 +150,7 @@ Flight::route("POST /inscription", function(){
         $st->execute(array($_POST['nom'],$_POST['email'],$motdepasse_chiffre));
         Flight::redirect('/success');
     }
-    });
+});
 
 /* 
 
@@ -152,6 +158,7 @@ CANDIDATURE
 
 */
 
+// Fonction de la route /candidature avec la méthode GET
 Flight::route('GET /candidature', function(){
     // On vérifie si l'utilisateur est connecté,
         if (isset($_SESSION['utilisateur'])){
@@ -160,7 +167,7 @@ Flight::route('GET /candidature', function(){
             $liste=$departement->fetchAll();
         
             $data = array(
-                "departements" => $liste
+                "departement" => $liste
             );
 
         Flight::render("candidature.tpl",$data);
@@ -170,10 +177,153 @@ Flight::route('GET /candidature', function(){
     }
 });
 
-
+// Fonction de la route /candidature avec la méthode POST
 Flight::route('POST /candidature', function(){
-    
+    $messages_erreur = array();
 
+    // Vérification du champ nomGroupe
+    if(empty($_POST['nomGroupe'])){
+        $messages_erreur['nomGroupe'] = "Le nom du groupe est obligatoire";
+    }
+
+    // Vérification du champ departement
+    if(empty($_POST['departement'])){
+        $messages_erreur['departement'] = "Le département est obligatoire";
+    }
+
+    // Vérification du champ typeScene
+    if(empty($_POST['typeScene'])){
+        $messages_erreur['typeScene'] = "Le type de scène est obligatoire";
+    }
+
+    // Vérification du champ representantGroupeNom
+    if(empty($_POST['representantGroupeNom'])){
+        $messages_erreur['representantGroupeNom'] = "Le nom du représentant du groupe est obligatoire";
+    }
+
+    // Vérification du champ representantGroupePrenom
+    if(empty($_POST['representantGroupePrenom'])){
+        $messages_erreur['representantGroupePrenom'] = "Le prénom du représentant du groupe est obligatoire";
+    }
+
+    // Vérification du champ representantGroupeAdresse
+    if(empty($_POST['representantGroupeAdresse'])){
+        $messages_erreur['representantGroupeAdresse'] = "L'adresse du représentant du groupe est obligatoire";
+    }
+
+    // Vérification du champ representantCodePostal
+    if(empty($_POST['representantGroupeCodePostal'])){
+        $messages_erreur['representantGroupeCodePostal'] = "Le code postal du représentant du groupe est obligatoire";
+    }
+
+    // Vérification du champ representantGroupeEmail
+    if(empty($_POST['representantGroupeEmail'])) {
+        $messages_erreur['representantGroupeEmail'] = "L'adresse mail est obligatoire";
+    }
+    elseif(!filter_var($_POST['representantGroupeEmail'], FILTER_VALIDATE_EMAIL)){
+        $messages_erreur['representantGroupeEmail'] = "Format d'adresse email non valide";
+    } else {
+        $db = Flight::get('db');
+        $st = $db->prepare("select representantGroupeEmail from candidature where email = ?");
+        $st->execute(array($_POST['representantGroupeEmail']));
+        if($st->rowCount()!==0){
+            $messages_erreur['representantGroupeeEmail']="L'adresse email est deja utilisee";
+        }
+    }
+
+    // Vérification du champ reprensetantGroupeTelephone
+    if(empty($_POST['representantGroupeTelephone'])){
+        $messages_erreur['representantGroupeTelephone'] = "Le numéro de téléphone est obligatoire";
+    }
+
+    // Vérification du champ styleMusical
+    if(empty($_POST['styleMusical'])){
+        $messages_erreur['styleMusical'] = "Le style musical est obligatoire";
+    }
+
+    // Vérification du champ anneeCreation
+    if(empty($_POST['anneeCreation'])){
+        $messages_erreur['anneeCreation'] = "L'année de création du groupe est obligatoire";
+    }
+
+    // Vérification du champ presentation
+    if(empty($_POST['presentation'])){
+        $messages_erreur['presentation'] = "Une présentation du groupe est obligatoire";
+    }
+
+    // Vérification du champ experience
+    if(empty($_POST['experience'])){
+        $messages_erreur['experience'] = "L'expérience sur scène du groupe est obligatoire";
+    }
+
+    // Vérification du champ siteOuPage
+    if(empty($_POST['siteOuPage'])){
+        $messages_erreur['SiteOuPage'] = "Un lien vers le site ou la page Facebook du groupe est obligatoire";
+    }
+
+    // Vérification du champ membres
+    if(empty($_POST['membres'])){
+        $messages_erreur['membres'] = "Les noms et prénoms des membres du groupe est obligatoire";
+    }
+
+    // Vérification du champ roles_membres
+    if(empty($_POST['roles_membres'])){
+        $messages_erreur['roles_membres'] = "Les rôles des membres du groupe est obligatoire";
+    }
+
+    // Vérification du champ fichierMP3
+    if(empty($_POST['fichierMP3'])){
+        $messages_erreur['fichierMP3'] = "3 Fichiers MP3 sont obligatoires";
+    }
+
+    // Vérification du champ photoGroupe
+    if(empty($_POST['photoGroupe'])){
+        $messages_erreur['photoGroupe'] = "Une photo du groupe est obligatoire";
+    }
+
+    // Vérification du champ ficheTechnique
+    if(empty($_POST['ficheTechnique'])){
+        $messages_erreur['ficheTechnique'] = "Une fiche technique en format PDF obligatoire";
+    }
+
+    // Envoi de messages d'erreurs si fautes de saisies
+    if(!empty($messages_erreur)) {
+        Flight::view()->assign("messages", $messages_erreur);
+        Flight::view()->assign($_POST);
+        Flight::render("candidature.tpl", NULL);
+    // Sinon envoi des informations saisies dans le formulaire vers la table candidature de la base de données
+    } else {
+        $get_db = Flight::get('db');
+        $st = $get_db->prepare('INSERT INTO candidature VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)');
+        $_POST['groupeNom'] = trim($_POST['groupeNom']);
+        $_POST['departement'] = trim($_POST['departement']);
+        $_POST['typeScene'] = trim($_POST['typeScene']);
+        $_POST['representantGroupeNom'] = trim($_POST['representantGroupeNom']);
+        $_POST['representantGroupePrenom'] = trim($_POST['representantGroupePrenom']);
+        $_POST['representantGroupeAdresse'] = trim($_POST['representantGroupeAdresse']);
+        $_POST['representantGroupeCodePostal'] = trim($_POST['representantGroupeCodePostal']);
+        $_POST['representantGroupeEmail'] = trim($_POST['representantGroupeEmail']);
+        $_POST['representantGroupeTelephone'] = trim($_POST['representantGroupeTelephone']);
+        $_POST['styleMusical'] = trim($_POST['styleMusical']);
+        $_POST['anneeCreation'] = trim($_POST['anneeCreation']);
+        $_POST['presentation'] = trim($_POST['presentation']);
+        $_POST['experience'] = trim($_POST['experience']);
+        $_POST['siteOuPage'] = trim($_POST['siteOuPage']);
+        $_POST['adresseSoundcloud'] = trim($_POST['adresseSoundcloud']);
+        $_POST['adresseYoutube'] = trim($_POST['adresseYoutube']);
+        $_POST['membres'] = trim($_POST['membres']);
+        $_POST['roles_membres'] = trim($_POST['roles_membres']);
+        $_POST['statutAssociatif'] = trim($_POST['statutAssociatif']);
+        $_POST['inscritSACEM'] = trim($_POST['inscritSACEM']);
+        $_POST['producteur'] = trim($_POST['producteur']);
+        $_POST['fichierMP3'] = trim($_POST['fichierMP3']);
+        $_POST['dossierPresse'] = trim($_POST['dossierPresse']);
+        $_POST['photoGroupe'] = trim($_POST['photoGroupe']);
+        $_POST['ficheTechnique'] = trim($_POST['ficheTechnique']);
+        $_POST['documentSACEM'] = trim($_POST['documentSACEM']);
+        $st->execute(array($_POST));
+        Flight::redirect('/success');
+    }
 });
 
 /*
@@ -221,8 +371,7 @@ DETAIL CANDIDATURE
 
 */
 
-Flight::route('GET /detail/@id', function() {
-	
+Flight::route('GET /detail/@id/@name', function() {
 	
 });
 
